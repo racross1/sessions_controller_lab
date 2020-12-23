548560ad8077d6e0906736688ef819cb70c4c3da
racross1 <racross1@gmail.com>
2020-12-23 11:51:17 -0800

    done

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+      if params[:name].blank?
+        redirect_to new_session_path
+      else 
+        session[:name] = params[:name]
+        redirect_to '/'
+      end 
+  end
+
+  def destroy
+    session.delete :name
+  end
+
+
+
+end
